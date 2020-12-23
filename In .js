a5be5f9753dad593e39ca09af63ca28cdfbb0667
Dhriti18 <73419650+Dhriti18@users.noreply.github.com>
2020-12-23 21:35:29 +0530

    Add files via upload

---
+++ In .js
@@ -0,0 +1,27 @@
+class Object{
+    constructor (x,y,width,height){
+    
+    
+    
+    this.body=Bodies.rectangle(x,y,width,height,{isStatic:true});
+   
+    
+    World.add(world,this.body);
+    
+    
+    
+    
+    
+    }
+    
+    display(){
+     
+     push ();
+      
+      rectMode(CENTER);
+    
+    pop ();
+    
+    
+    
+    }}
